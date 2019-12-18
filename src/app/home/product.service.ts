bf40d7384efaebfb2a6c00392a8b432878afc474
farhana <farhanamananthery@gmail.com>
2019-12-18 21:35:29 +0530

    first commit

--- src/app/home/product.service.ts
+++ src/app/home/product.service.ts
@@ -16,48 +16,45 @@ export class ProductService {
     return this.storage.get('id');
   }
   login(user){
-    return this.http.post("http://localhost:3000/login",user)
+    return this.http.post("http://localhost:8080/login",user)
   }
   signup(user){
-    return this.http.post("http://localhost:3000/signup",user)
+    return this.http.post("http://localhost:8080/signup",user)
   }
   users(){
-    return this.http.get("http://localhost:3000/signup/users")
+    return this.http.get("http://localhost:8080/signup/users")
   }
-  // user(id){
-  //   return this.http.post("http://localhost:3000/signup/userDetails",{id:id})
-  // }
   deleteUser(email){
-    return this.http.post("http://localhost:3000/signup/delete",{email:email})
+    return this.http.post("http://localhost:8080/signup/delete",{email:email})
   }
   products(category){ 
-    return this.http.post("http://localhost:3000/products",{category:category})
+    return this.http.post("http://localhost:8080/products",{category:category})
   }
   newProduct(product){
-    return this.http.post("http://localhost:3000/products/insert",product)  
+    return this.http.post("http://localhost:8080/products/insert",product)  
   }
   updateProduct(product){
-    return this.http.post("http://localhost:3000/products/update",product) 
+    return this.http.post("http://localhost:8080/products/update",product) 
   }
   delete(id){
-    return this.http.post("http://localhost:3000/products/delete",{id:id}) 
+    return this.http.post("http://localhost:8080/products/delete",{id:id}) 
   }
   setUser(email){
     this.storage.set('email',email);
   }
   getUser(user){
-    return this.http.post("http://localhost:3000/signup/profile",{email:user})
+    return this.http.post("http://localhost:8080/signup/profile",{email:user})
   }
   getAds(user){
-    return this.http.post("http://localhost:3000/products/ads",{email:user})
+    return this.http.post("http://localhost:8080/products/ads",{email:user})
   }
   product(id){
-    return this.http.post("http://localhost:3000/products/product",{id:id}) 
+    return this.http.post("http://localhost:8080/products/product",{id:id}) 
   }
   random(){
-    return this.http.get("http://localhost:3000/products/random") 
+    return this.http.get("http://localhost:8080/products/random") 
   }
 } 
 
 
-  
+    
